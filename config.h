5c243ead595229faf6fa78e4f1a1588fa86afc94
anonymous22 <anonymous22@gmail.com>
2021-08-27 20:48:59 +0000

    incremental build of ix64 branch with 00010

--- config.h
+++ config.h
@@ -19,6 +19,7 @@
 #define HAVE_BITDEPTH8 1
 #define HAVE_BITDEPTH10 1
 #define CHROMA_FORMAT CHROMA_444
+#define HAVE_GPL 1
 #define HAVE_INTERLACED 1
 #define HAVE_OPENCL (BIT_DEPTH==8)
 #define HAVE_ALTIVEC 0
@@ -33,7 +34,6 @@
 #define HAVE_WIN32THREAD 0
 #define HAVE_THREAD 0
 #define HAVE_GPAC 0
-#define HAVE_GPL 0
 #define HAVE_CPU_COUNT 0
 #define HAVE_X86_INLINE_ASM 0
 #define HAVE_AS_FUNC 0
