a2dc2071047b7e5667265c1347fed86aba6237fb
anonymous22 <anonymous22@gmail.com>
2021-08-27 20:59:37 +0000

    incremental build of ix64 branch with 00014

--- x264_config.h
+++ x264_config.h
@@ -1,6 +1,8 @@
-#define X264_GPL           1
+#define X264_GPL           0
 #define X264_INTERLACED    0
 #define X264_BIT_DEPTH     8
-#define X264_CHROMA_FORMAT X264_CSP_I400
-#define X264_VERSION ""
-#define X264_POINTVER "0.164.x"
+#define X264_CHROMA_FORMAT X264_CSP_I420
+#define X264_REV 2
+#define X264_REV_DIFF 0
+#define X264_VERSION " r2M dab0be6"
+#define X264_POINTVER "0.164.2M dab0be6"
