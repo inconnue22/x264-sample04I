d377fb56a0d92fd7990b827c1044c655f1498275
anonymous22 <anonymous22@gmail.com>
2021-08-27 21:14:09 +0000

    incremental build of ix64 branch with 00005

--- config.h
+++ config.h
@@ -4,9 +4,6 @@
 #define ARCH_X86_64 1
 #define SYS_LINUX 1
 #define STACK_ALIGNMENT 64
-#define HAVE_POSIXTHREAD 1
-#define HAVE_CPU_COUNT 1
-#define HAVE_THREAD 1
 #define HAVE_LOG2F 1
 #define HAVE_STRTOK_R 1
 #define HAVE_CLOCK_GETTIME 1
@@ -22,8 +19,7 @@
 #define fseek fseeko
 #define ftell ftello
 #define HAVE_BITDEPTH8 1
-#define CHROMA_FORMAT CHROMA_444
-#define HAVE_GPL 1
+#define HAVE_BITDEPTH10 1
 #define HAVE_INTERLACED 1
 #define HAVE_OPENCL (BIT_DEPTH==8)
 #define HAVE_ALTIVEC 0
@@ -33,12 +29,15 @@
 #define HAVE_NEON 0
 #define HAVE_AARCH64 0
 #define HAVE_BEOSTHREAD 0
+#define HAVE_POSIXTHREAD 0
 #define HAVE_WIN32THREAD 0
+#define HAVE_THREAD 0
 #define HAVE_GPAC 0
+#define HAVE_GPL 0
+#define HAVE_CPU_COUNT 0
 #define HAVE_AS_FUNC 0
 #define HAVE_INTEL_DISPATCHER 0
 #define HAVE_MSA 0
 #define HAVE_WINRT 0
 #define HAVE_VSX 0
 #define HAVE_ARM_INLINE_ASM 0
-#define HAVE_BITDEPTH10 0
