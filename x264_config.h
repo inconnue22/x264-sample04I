13d4cfd5029abf779882c559d689cf722a2320d0
anonymous22 <anonymous22@gmail.com>
2021-08-27 17:05:04 +0000

    clean build x264 master copy with 00012

---
+++ x264_config.h
@@ -0,0 +1,6 @@
+#define X264_GPL           1
+#define X264_INTERLACED    1
+#define X264_BIT_DEPTH     0
+#define X264_CHROMA_FORMAT X264_CSP_I400
+#define X264_VERSION ""
+#define X264_POINTVER "0.164.x"
